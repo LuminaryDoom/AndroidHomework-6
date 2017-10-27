ebb18cd5471bfc8c62c42e1c7d03b22e20595fbd
bRad-95 <bryantramirez@ac.c4q.nyc>
2017-10-27 11:28:40 -0400

    current commit

---
+++ app/src/main/java/nyc/c4q/scientificcalculator/Calculate.java
@@ -0,0 +1,18 @@
+package nyc.c4q.scientificcalculator;
+
+/**
+ * Created by c4q on 10/27/17.
+ */
+
+public class Calculate {
+
+    public void getAnswer()
+    {
+
+
+
+
+    }
+
+
+}
--- app/src/main/java/nyc/c4q/scientificcalculator/MainActivity.java
+++ app/src/main/java/nyc/c4q/scientificcalculator/MainActivity.java
@@ -10,59 +10,16 @@ import org.mariuszgromada.math.mxparser.*;
 public class MainActivity extends AppCompatActivity {
 
     TextView displayText;
-    /**
-     * Vertical Buttons
-     */
-    Button leftParentheses;
-    Button rightParentheses;
-    Button percent;
-    Button decimalPoint;
 
-    Button allClear;
-    Button clearEntry;
-
-    Button numberOne;
-    Button numberTwo;
-    Button numberThree;
-    Button numberFour;
-    Button numberFive;
-    Button numberSix;
-    Button numberSeven;
-    Button numberEight;
-    Button numberNine;
-    Button numberZero;
-
-    Button divide;
-    Button multiply;
-    Button subtract;
-    Button plus;
+    Button allClear; //delete
     Button equals;
-
-    /**
-     * Horizontal Buttons
-     */
-    Button factorial;
-
     Button inverse;
-    Button sine;
-    Button inverseSine;
-    Button cosine;
-    Button inverseCosine;
-    Button tan;
-    Button inverseTan;
+    Button Ans; //stored last answers
+    Button Rnd; //Random number
+    Button Rad; //parse to radian
+    Button Deg; //set to degree
 
-    Button naturalLog;
-    Button logarithim;
-    Button pi;
-    Button eulersNumber;
-    Button eulersNumberPowerX;
-    Button squareRoot;
-    Button exponential;
-    Button power;
-    Button rnd;
-    Button tenPowerX;
-    Button square;
-    Button xRoot;
+
 
 
     @Override
@@ -81,29 +38,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Expression e1 =new Expression("5!");
+        Expression e1 =new Expression("deg(5.31)");
         double answer = e1.calculate();
         displayText = (TextView) findViewById(R.id.math_view);
         displayText.setText(Double.toString(answer));
 
 
-        divide = (Button) findViewById(R.id.button_division);
-        divide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                storeString.mathText += "/";
-                displayText.setText(storeString.mathText);
-            }
-        });
-
-        inverse = (Button) findViewById(R.id.button_inverse);
-        inverse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sine = (Button) findViewById(R.id.button_sin);
-                sine.setText("asin");
-            }
-        });
     }
 
     public void getText(View v)
@@ -130,5 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
         displayText.setText(storeString.mathText);
     }
+
+
 
 }
